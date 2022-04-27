02fad038435083ba3a28b0047cbf1f015a286cb9
CalebMueller-UH <mc61@hawaii.edu>
2022-04-27 11:41:07 -1000

    code formatting in weight

--- Weight.cpp
+++ Weight.cpp
@@ -12,50 +12,44 @@
 
 /////////////////////////////////// Constants //////////////////////////////////////
 const t_weight Weight::UNKNOWN_WEIGHT = 0;
+
 const t_weight Weight::KILOS_IN_A_POUND = 0.453592;
+
 const t_weight Weight::SLUGS_IN_A_POUND = 0.031081;
+
 const Weight::UnitOfWeight Weight::DEFAULT_UNIT_OF_WEIGHT = POUNDS;
+
 const t_weight Weight::DEFAULT_MAX_WEIGHT = 40;
+
 const string Weight::POUND_LITERAL = "lbs";
+
 const string Weight::SLUG_LITERAL = "slugs";
+
 const string Weight::KILO_LITERAL = "kgs";
 
 /////////////////////////////////// Class Constructors //////////////////////////////////////
 // ↓ #1 Default Constructor: delegates to #7 with all default values
-Weight::Weight() noexcept
-		:Weight(UNKNOWN_WEIGHT, DEFAULT_UNIT_OF_WEIGHT, UNKNOWN_WEIGHT)
-{ }
+Weight::Weight() noexcept : Weight( UNKNOWN_WEIGHT, DEFAULT_UNIT_OF_WEIGHT, UNKNOWN_WEIGHT ){}
 
 // ↓ #2 only newWeight parameter provided: delegates to #7 with default values for _unitOfWeight and _maxWeight
-Weight::Weight(t_weight newWeight)
-		:Weight(newWeight, DEFAULT_UNIT_OF_WEIGHT, UNKNOWN_WEIGHT)
-{ }
+Weight::Weight( t_weight newWeight ) : Weight( newWeight, DEFAULT_UNIT_OF_WEIGHT, UNKNOWN_WEIGHT ){}
 
 // ↓ #3 Only newUnitOfWeight parameter provided: delegates to #7 with default values for _weight and _unitOfWeight
-Weight::Weight(Weight::UnitOfWeight newUnitOfWeight) noexcept
-		:Weight(UNKNOWN_WEIGHT, newUnitOfWeight, UNKNOWN_WEIGHT)
-{ }
+Weight::Weight( Weight::UnitOfWeight newUnitOfWeight ) noexcept : Weight( UNKNOWN_WEIGHT, newUnitOfWeight, UNKNOWN_WEIGHT ){}
 
 // ↓ #4 newWeight and newUnitOfWeight parameters provided: delegates to #7 with default value for _maxWeight
-Weight::Weight(t_weight newWeight, Weight::UnitOfWeight newUnitOfWeight) noexcept
-		:Weight(newWeight, newUnitOfWeight, UNKNOWN_WEIGHT)
-{ }
+Weight::Weight( t_weight newWeight, Weight::UnitOfWeight newUnitOfWeight ) noexcept : Weight( newWeight, newUnitOfWeight, UNKNOWN_WEIGHT ){}
 
 // ↓ #5  newWeight and newMaxWeight provided: delegates to #7 with default value for _unitOfWeight
-Weight::Weight(t_weight newWeight, t_weight newMaxWeight) noexcept
-		:Weight(newWeight, DEFAULT_UNIT_OF_WEIGHT, newMaxWeight)
-{ }
+Weight::Weight( t_weight newWeight, t_weight newMaxWeight ) noexcept : Weight( newWeight, DEFAULT_UNIT_OF_WEIGHT, newMaxWeight ){}
 
 // ↓ #6  newUnitOfWeight and newMaxWeight provided: delegates to #7 with default value for _weight
-Weight::Weight(UnitOfWeight newUnitOfWeight, t_weight newMaxWeight) noexcept
-		:Weight(UNKNOWN_WEIGHT, newUnitOfWeight, newMaxWeight)
-{ }
+Weight::Weight( UnitOfWeight newUnitOfWeight, t_weight newMaxWeight ) noexcept : Weight( UNKNOWN_WEIGHT, newUnitOfWeight, newMaxWeight ){}
 
 // ↓ #7  all member variables provided as parameters
-Weight::Weight(t_weight newWeight, Weight::UnitOfWeight newUnitOfWeight, t_weight newMaxWeight) noexcept
-		:_unitOfWeight{newUnitOfWeight}
+Weight::Weight( t_weight newWeight, Weight::UnitOfWeight newUnitOfWeight, t_weight newMaxWeight ) noexcept : _unitOfWeight{newUnitOfWeight}
 {
-	if (maxWeightIsValid(newMaxWeight))
+	if( maxWeightIsValid( newMaxWeight ) )
 	{
 		_maxWeight = newMaxWeight;
 		_bHasMax = true;
@@ -70,7 +64,7 @@ Weight::Weight(t_weight newWeight, Weight::UnitOfWeight newUnitOfWeight, t_weigh
 		#endif
 	}
 
-	if (weightIsValid(newWeight, _maxWeight))
+	if( weightIsValid( newWeight, _maxWeight ) )
 	{
 		_weight = newWeight;
 		_bIsKnown = true;
@@ -82,77 +76,90 @@ Weight::Weight(t_weight newWeight, Weight::UnitOfWeight newUnitOfWeight, t_weigh
 }
 
 /////////////////////////////////// Static Methods //////////////////////////////////////
-t_weight Weight::fromKilogramsToPounds(const t_weight weightInKilos) noexcept
+t_weight Weight::fromKilogramsToPounds( const t_weight weightInKilos ) noexcept
 {
 	return weightInKilos / KILOS_IN_A_POUND;
 }
 
-t_weight Weight::fromPoundsToKilograms(const t_weight weightInPounds) noexcept
+t_weight Weight::fromPoundsToKilograms( const t_weight weightInPounds ) noexcept
 {
 	return weightInPounds * KILOS_IN_A_POUND;
 }
 
-t_weight Weight::fromSlugsToPounds(const t_weight weightInSlugs) noexcept
+t_weight Weight::fromSlugsToPounds( const t_weight weightInSlugs ) noexcept
 {
 	return weightInSlugs / SLUGS_IN_A_POUND;
 }
 
-t_weight Weight::fromPoundsToSlugs(const t_weight weightInPounds) noexcept
+t_weight Weight::fromPoundsToSlugs( const t_weight weightInPounds ) noexcept
 {
 	return weightInPounds * SLUGS_IN_A_POUND;
 }
 
-t_weight Weight::convertWeight(const t_weight fromWeight, const Weight::UnitOfWeight fromUnit, const Weight::UnitOfWeight toUnit) noexcept
+t_weight Weight::convertWeight( const t_weight fromWeight, const Weight::UnitOfWeight fromUnit, const Weight::UnitOfWeight toUnit ) noexcept
 {
-	switch (fromUnit)
+	switch( fromUnit )
 	{
-	case POUNDS:
-		switch (toUnit)
-		{
-		case POUNDS: return fromWeight;
-		case SLUGS: return fromPoundsToSlugs(fromWeight);
-		case KILOS: return fromPoundsToKilograms(fromWeight);
-		default: assert(0);
-		}
+		case POUNDS:
+			switch( toUnit )
+			{
+				case POUNDS:
+					return fromWeight;
+				case SLUGS:
+					return fromPoundsToSlugs( fromWeight );
+				case KILOS:
+					return fromPoundsToKilograms( fromWeight );
+				default:
+					assert( 0 );
+			}
 
-	case SLUGS:
-		switch (toUnit)
-		{
-		case POUNDS: return fromSlugsToPounds(fromWeight);
-		case SLUGS: return fromWeight;
-		case KILOS: return fromPoundsToKilograms(fromSlugsToPounds(fromWeight));
-		default: assert(0);
-		}
+		case SLUGS:
+			switch( toUnit )
+			{
+				case POUNDS:
+					return fromSlugsToPounds( fromWeight );
+				case SLUGS:
+					return fromWeight;
+				case KILOS:
+					return fromPoundsToKilograms( fromSlugsToPounds( fromWeight ) );
+				default:
+					assert( 0 );
+			}
 
-	case KILOS:
-		switch (toUnit)
-		{
-		case POUNDS: return fromKilogramsToPounds(fromWeight);
-		case SLUGS: return fromPoundsToSlugs(fromKilogramsToPounds(fromWeight));
-		case KILOS: return fromWeight;
-		default: assert(0);
-		}
-	default: assert(0);
+		case KILOS:
+			switch( toUnit )
+			{
+				case POUNDS:
+					return fromKilogramsToPounds( fromWeight );
+				case SLUGS:
+					return fromPoundsToSlugs( fromKilogramsToPounds( fromWeight ) );
+				case KILOS:
+					return fromWeight;
+				default:
+					assert( 0 );
+			}
+		default:
+			assert( 0 );
 	}
 }
 
 /////////////////////////////////// Getters ///////////////////////////////////
 t_weight Weight::getWeight() const noexcept
 {
-	if (_bIsKnown)
+	if( _bIsKnown )
 	{
 		return _weight;
 	}
-	assert(0); //invalid weight encountered, force crash
+	assert( 0 ); //invalid weight encountered, force crash
 }
 
-t_weight Weight::getWeight(const Weight::UnitOfWeight weightUnits) const noexcept
+t_weight Weight::getWeight( const Weight::UnitOfWeight weightUnits ) const noexcept
 {
-	if (_bIsKnown)
+	if( _bIsKnown )
 	{
-		return convertWeight(_weight, _unitOfWeight, weightUnits);
+		return convertWeight( _weight, _unitOfWeight, weightUnits );
 	}
-	assert(0); //invalid weight encountered, force crash
+	assert( 0 ); //invalid weight encountered, force crash
 }
 
 t_weight Weight::getMaxWeight() const noexcept
@@ -166,27 +173,27 @@ Weight::UnitOfWeight Weight::getUnitOfWeight() const noexcept
 }
 
 /////////////////////////////////// Setters ///////////////////////////////////
-void Weight::setWeight(const t_weight newWeight)
+void Weight::setWeight( const t_weight newWeight )
 {
-	if (weightIsValid(newWeight, _maxWeight))
+	if( weightIsValid( newWeight, _maxWeight ) )
 	{
 		_bIsKnown = true;
 		_weight = newWeight;
 	}
 }
 
-void Weight::setWeight(const t_weight newWeight, Weight::UnitOfWeight weightUnits)
+void Weight::setWeight( const t_weight newWeight, Weight::UnitOfWeight weightUnits )
 {
-	if (weightIsValid(newWeight, _maxWeight))
+	if( weightIsValid( newWeight, _maxWeight ) )
 	{
-		_weight = convertWeight(newWeight, _unitOfWeight, weightUnits);
+		_weight = convertWeight( newWeight, _unitOfWeight, weightUnits );
 	}
 }
 
-void Weight::setMaxWeight(const t_weight newMaxWeight)
+void Weight::setMaxWeight( const t_weight newMaxWeight )
 {
 	// Can only set _maxWeight if it isn't already set    ↓
-	if (maxWeightIsValid(newMaxWeight) && !_bHasMax)
+	if( maxWeightIsValid( newMaxWeight ) && !_bHasMax )
 	{
 		_maxWeight = newMaxWeight;
 		_bHasMax = true;
@@ -206,21 +213,21 @@ bool Weight::hasMaxWeight() const noexcept
 
 void Weight::dump() const noexcept
 {
-	FORMAT_LINE_FOR_DUMP("Weight", "this") << this << std::endl;
-	FORMAT_LINE_FOR_DUMP("Weight", "isKnown") << _bIsKnown << std::endl;
-	FORMAT_LINE_FOR_DUMP("Weight", "weight") << _weight << std::endl;
-	FORMAT_LINE_FOR_DUMP("Weight", "unitOfWeight") << _unitOfWeight << std::endl;
-	FORMAT_LINE_FOR_DUMP("Weight", "hasMax") << _bHasMax << std::endl;
-	FORMAT_LINE_FOR_DUMP("Weight", "maxWeight") << _maxWeight << std::endl;
+	FORMAT_LINE_FOR_DUMP( "Weight", "this" ) << this << std::endl;
+	FORMAT_LINE_FOR_DUMP( "Weight", "isKnown" ) << _bIsKnown << std::endl;
+	FORMAT_LINE_FOR_DUMP( "Weight", "weight" ) << _weight << std::endl;
+	FORMAT_LINE_FOR_DUMP( "Weight", "unitOfWeight" ) << _unitOfWeight << std::endl;
+	FORMAT_LINE_FOR_DUMP( "Weight", "hasMax" ) << _bHasMax << std::endl;
+	FORMAT_LINE_FOR_DUMP( "Weight", "maxWeight" ) << _maxWeight << std::endl;
 }
 
 /////////////////////////////////// Validation Methods ///////////////////////////////////
-bool Weight::weightIsValid(const t_weight checkWeight, const t_weight checkMaxWeight) noexcept
+bool Weight::weightIsValid( const t_weight checkWeight, const t_weight checkMaxWeight ) noexcept
 {
 	return (checkWeight > 0 && checkWeight != UNKNOWN_WEIGHT && checkWeight <= checkMaxWeight);
 }
 
-bool Weight::maxWeightIsValid(const t_weight checkMaxWeight) noexcept
+bool Weight::maxWeightIsValid( const t_weight checkMaxWeight ) noexcept
 {
 	return (checkMaxWeight > 0 && checkMaxWeight < DEFAULT_MAX_WEIGHT);
 }
@@ -231,55 +238,59 @@ bool Weight::validate() const noexcept
 }
 
 /////////////////////////////////// Overloaded Operators ///////////////////////////////////
-bool Weight::operator==(const Weight& rhs) const
+bool Weight::operator==( const Weight &rhs ) const
 {
-	t_weight lhs_weight = (_bIsKnown) ? getWeight(Weight::POUNDS) : 0;
-	t_weight rhs_weight = (rhs._bIsKnown) ? rhs.getWeight(Weight::POUNDS) : 0;
+	t_weight lhs_weight = (_bIsKnown) ? getWeight( Weight::POUNDS ) : 0;
+	t_weight rhs_weight = (rhs._bIsKnown) ? rhs.getWeight( Weight::POUNDS ) : 0;
 	return lhs_weight == rhs_weight;
 }
 
-bool Weight::operator<(const Weight& rhs) const
+bool Weight::operator<( const Weight &rhs ) const
 {
-	t_weight lhs_weight = (_bIsKnown) ? getWeight(Weight::POUNDS) : 0;
-	t_weight rhs_weight = (rhs._bIsKnown) ? rhs.getWeight(Weight::POUNDS) : 0;
+	t_weight lhs_weight = (_bIsKnown) ? getWeight( Weight::POUNDS ) : 0;
+	t_weight rhs_weight = (rhs._bIsKnown) ? rhs.getWeight( Weight::POUNDS ) : 0;
 	return lhs_weight < rhs_weight;
 }
 
-Weight& Weight::operator+=(const t_weight& rhs_weight)
+Weight &Weight::operator+=( const t_weight &rhs_weight )
 {
-	t_weight lhs_weight = (_bIsKnown) ? getWeight(Weight::POUNDS) : 0;
+	t_weight lhs_weight = (_bIsKnown) ? getWeight( Weight::POUNDS ) : 0;
 	t_weight sum = lhs_weight + rhs_weight;
-	if (!weightIsValid(sum, _maxWeight))
+	if( !weightIsValid( sum, _maxWeight ) )
 	{
-		throw invalid_argument(PROGRAM_NAME
-							   " += operation results is an invalid weight!");
+		throw invalid_argument( PROGRAM_NAME
+		                        " += operation results is an invalid weight!" );
 	}
-	setWeight(sum);
+	setWeight( sum );
 	return *this;
 }
 
 /////////////////////////////////// Externally Scoped Overloaded Operators ///////////////////////////////////
-std::ostream& operator<<(ostream& lhs_stream, const Weight::UnitOfWeight rhs_UnitOfWeight)
+std::ostream &operator<<( ostream &lhs_stream, const Weight::UnitOfWeight rhs_UnitOfWeight )
 {
-	switch (rhs_UnitOfWeight)
+	switch( rhs_UnitOfWeight )
 	{
-	case Weight::POUNDS: return lhs_stream << Weight::POUND_LITERAL;
-	case Weight::KILOS: return lhs_stream << Weight::KILO_LITERAL;
-	case Weight::SLUGS: return lhs_stream << Weight::SLUG_LITERAL;
-	default: throw out_of_range(PROGRAM_NAME " Weight Error: The unit can not be converted to a string!");
+		case Weight::POUNDS:
+			return lhs_stream << Weight::POUND_LITERAL;
+		case Weight::KILOS:
+			return lhs_stream << Weight::KILO_LITERAL;
+		case Weight::SLUGS:
+			return lhs_stream << Weight::SLUG_LITERAL;
+		default:
+			throw out_of_range( PROGRAM_NAME " Weight Error: The unit can not be converted to a string!" );
 	}
 }
 
-std::ostream& operator<<(std::ostream& lhs_stream, const Weight rhs_Weight)
+std::ostream &operator<<( std::ostream &lhs_stream, const Weight rhs_Weight )
 {
 	stringstream stringBuffer;
 
-	if (!rhs_Weight.weightIsKnown() && !rhs_Weight.hasMaxWeight())
+	if( !rhs_Weight.weightIsKnown() && !rhs_Weight.hasMaxWeight() )
 	{
 		lhs_stream << "Unknown";
 		return lhs_stream;
 	}
-	else if (rhs_Weight.weightIsKnown())
+	else if( rhs_Weight.weightIsKnown() )
 	{
 		stringBuffer << rhs_Weight.getWeight();
 	}
@@ -288,7 +299,7 @@ std::ostream& operator<<(std::ostream& lhs_stream, const Weight rhs_Weight)
 		stringBuffer << "Unknown";
 	}
 
-	if (rhs_Weight.hasMaxWeight())
+	if( rhs_Weight.hasMaxWeight() )
 	{
 		stringBuffer << " out of " << rhs_Weight.getMaxWeight();
 	}
--- Weight.h
+++ Weight.h
@@ -31,102 +31,100 @@ typedef float t_weight;
 
 class Weight
 {
-public:  /////////////////////////////////// Enumerations ///////////////////////////////////
-	enum UnitOfWeight
-	{
-		POUNDS,
-		KILOS,
-		SLUGS
-	};
+  public:  /////////////////////////////////// Enumerations ///////////////////////////////////
+  enum UnitOfWeight
+  {
+	POUNDS,
+	KILOS,
+	SLUGS
+  };
 
-protected:  /////////////////////////////////// Member Variables ///////////////////////////////////
-	UnitOfWeight _unitOfWeight{ DEFAULT_UNIT_OF_WEIGHT };
-	t_weight _weight{ UNKNOWN_WEIGHT };
-	t_weight _maxWeight{ DEFAULT_MAX_WEIGHT };
-	bool _bIsKnown{ false }; ///< bool flag for if _weight has been set to a valid value
-	bool _bHasMax{ false }; ///< bool flag for if _maxWeight has been set to a valid value
+  protected:  /////////////////////////////////// Member Variables ///////////////////////////////////
+  UnitOfWeight _unitOfWeight{DEFAULT_UNIT_OF_WEIGHT};
+  t_weight _weight{UNKNOWN_WEIGHT};
+  t_weight _maxWeight{DEFAULT_MAX_WEIGHT};
+  bool _bIsKnown{false}; ///< bool flag for if _weight has been set to a valid value
+  bool _bHasMax{false}; ///< bool flag for if _maxWeight has been set to a valid value
 
-public:  /////////////////////////////////// Constants ///////////////////////////////////
-	static const t_weight UNKNOWN_WEIGHT;
-	static const t_weight KILOS_IN_A_POUND;
-	static const t_weight SLUGS_IN_A_POUND;
-	static const UnitOfWeight DEFAULT_UNIT_OF_WEIGHT;
-	static const t_weight DEFAULT_MAX_WEIGHT;
-	static const string POUND_LITERAL;
-	static const string SLUG_LITERAL;
-	static const string KILO_LITERAL;
+  public:  /////////////////////////////////// Constants ///////////////////////////////////
+  static const t_weight UNKNOWN_WEIGHT;
+  static const t_weight KILOS_IN_A_POUND;
+  static const t_weight SLUGS_IN_A_POUND;
+  static const UnitOfWeight DEFAULT_UNIT_OF_WEIGHT;
+  static const t_weight DEFAULT_MAX_WEIGHT;
+  static const string POUND_LITERAL;
+  static const string SLUG_LITERAL;
+  static const string KILO_LITERAL;
 
-public:  /////////////////////////////////// Class Constructors ///////////////////////////////////
-	// ↓ #1 Default Constructor: delegates to #7 with all default values
-	Weight() noexcept;
+  public:  /////////////////////////////////// Class Constructors ///////////////////////////////////
+  // ↓ #1 Default Constructor: delegates to #7 with all default values
+  Weight() noexcept;
 
-	// ↓ #2 only newWeight parameter provided: delegates to #7 with default values for _unitOfWeight and _maxWeight
-	explicit Weight( t_weight newWeight );
+  // ↓ #2 only newWeight parameter provided: delegates to #7 with default values for _unitOfWeight and _maxWeight
+  explicit Weight( t_weight newWeight );
 
-	// ↓ #3 Only newUnitOfWeight parameter provided: delegates to #7 with default values for _weight and _unitOfWeight
-	explicit Weight( UnitOfWeight newUnitOfWeight ) noexcept;
+  // ↓ #3 Only newUnitOfWeight parameter provided: delegates to #7 with default values for _weight and _unitOfWeight
+  explicit Weight( UnitOfWeight newUnitOfWeight ) noexcept;
 
-	// ↓ #4 newWeight and newUnitOfWeight parameters provided: delegates to #7 with default value for _maxWeight
-	Weight( t_weight newWeight, UnitOfWeight newUnitOfWeight ) noexcept;
+  // ↓ #4 newWeight and newUnitOfWeight parameters provided: delegates to #7 with default value for _maxWeight
+  Weight( t_weight newWeight, UnitOfWeight newUnitOfWeight ) noexcept;
 
-	// ↓ #5  newWeight and newMaxWeight provided: delegates to #7 with default value for _unitOfWeight
-	Weight( t_weight newWeight, t_weight newMaxWeight ) noexcept;
+  // ↓ #5  newWeight and newMaxWeight provided: delegates to #7 with default value for _unitOfWeight
+  Weight( t_weight newWeight, t_weight newMaxWeight ) noexcept;
 
-	// ↓ #6  newUnitOfWeight and newMaxWeight provided: delegates to #7 with default value for _weight
-	Weight( UnitOfWeight newUnitOfWeight, t_weight newMaxWeight ) noexcept;
+  // ↓ #6  newUnitOfWeight and newMaxWeight provided: delegates to #7 with default value for _weight
+  Weight( UnitOfWeight newUnitOfWeight, t_weight newMaxWeight ) noexcept;
 
-	// ↓ #7  all member variables provided as parameters
-	Weight( t_weight newWeight, UnitOfWeight newUnitOfWeight, t_weight newMaxWeight ) noexcept;
+  // ↓ #7  all member variables provided as parameters
+  Weight( t_weight newWeight, UnitOfWeight newUnitOfWeight, t_weight newMaxWeight ) noexcept;
 
-public:  /////////////////////////////////// Static Methods ///////////////////////////////////
-	static t_weight fromKilogramsToPounds( const t_weight weightInKilos ) noexcept;
+  public:  /////////////////////////////////// Static Methods ///////////////////////////////////
+  static t_weight fromKilogramsToPounds( const t_weight weightInKilos ) noexcept;
 
-	static t_weight fromPoundsToKilograms( const t_weight weightInPounds ) noexcept;
+  static t_weight fromPoundsToKilograms( const t_weight weightInPounds ) noexcept;
 
-	static t_weight fromSlugsToPounds( const t_weight weightInSlugs ) noexcept;
+  static t_weight fromSlugsToPounds( const t_weight weightInSlugs ) noexcept;
 
-	static t_weight fromPoundsToSlugs( const t_weight weightInPounds ) noexcept;
+  static t_weight fromPoundsToSlugs( const t_weight weightInPounds ) noexcept;
 
-	static t_weight convertWeight( const t_weight fromWeight, const UnitOfWeight fromUnit, const UnitOfWeight toUnit ) noexcept;
+  static t_weight convertWeight( const t_weight fromWeight, const UnitOfWeight fromUnit, const UnitOfWeight toUnit ) noexcept;
 
-public:    /////////////////////////////////// Getters ///////////////////////////////////
-	t_weight getWeight() const noexcept;
+  public:    /////////////////////////////////// Getters ///////////////////////////////////
+  t_weight getWeight() const noexcept;
 
-	t_weight getWeight( const UnitOfWeight weightUnits ) const noexcept;
+  t_weight getWeight( const UnitOfWeight weightUnits ) const noexcept;
 
-	t_weight getMaxWeight() const noexcept;
+  t_weight getMaxWeight() const noexcept;
 
-	UnitOfWeight getUnitOfWeight() const noexcept;
+  UnitOfWeight getUnitOfWeight() const noexcept;
 
-public: /////////////////////////////////// Setters ///////////////////////////////////
-	void setWeight( const t_weight newWeight );
+  public: /////////////////////////////////// Setters ///////////////////////////////////
+  void setWeight( const t_weight newWeight );
 
-	void setWeight( const t_weight newWeight, UnitOfWeight weightUnits );
+  void setWeight( const t_weight newWeight, UnitOfWeight weightUnits );
 
+  public:    /////////////////////////////////// Public Methods ///////////////////////////////////
+  bool weightIsKnown() const noexcept;
 
-public:    /////////////////////////////////// Public Methods ///////////////////////////////////
-	bool weightIsKnown() const noexcept;
+  bool hasMaxWeight() const noexcept;
 
-	bool hasMaxWeight() const noexcept;
+  void dump() const noexcept;
 
-	void dump() const noexcept;
+  void setMaxWeight( const t_weight newMaxWeight );
 
-	void setMaxWeight( const t_weight newMaxWeight );
+  public:    /////////////////////////////////// Validation Methods ///////////////////////////////////
+  static bool weightIsValid( const t_weight checkWeight, const t_weight checkMaxWeight ) noexcept;
 
-public:    /////////////////////////////////// Validation Methods ///////////////////////////////////
-	static bool weightIsValid( const t_weight checkWeight, const t_weight checkMaxWeight ) noexcept;
+  static bool maxWeightIsValid( const t_weight checkMaxWeight ) noexcept;
 
-	static bool maxWeightIsValid( const t_weight checkMaxWeight ) noexcept;
+  bool validate() const noexcept;
 
-	bool validate() const noexcept;
+  public:  /////////////////////////////////// Overloaded Operators ///////////////////////////////////
+  bool operator==( const Weight &rhs ) const;
 
-public:  /////////////////////////////////// Overloaded Operators ///////////////////////////////////
-	bool operator==( const Weight &rhs ) const;
+  bool operator<( const Weight &rhs ) const;
 
-	bool operator<( const Weight &rhs ) const;
-
-	Weight &operator+=( const t_weight &rhs_weight );
-
+  Weight &operator+=( const t_weight &rhs_weight );
 };
 
 /////////////////////////////////// Externally Scoped Overloaded Operators ///////////////////////////////////
